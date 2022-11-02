c3f297418706dcfe965199b615ef681f4be48836
Varissara <varissara.wo@gmail.com>
2022-11-02 20:18:14 +0700

    feat: update form section

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -16,22 +16,52 @@ import {
   InputLabel,
 } from "@mui/material";
 
+import { FileUploadOutlined } from "@mui/icons-material";
+
 const ButtonStyle = styled(Button)(() => ({
   fontFamily: "var( --inter-font)",
   fontWeight: "500",
   fontSize: "14px",
   borderRadius: "16px",
   padding: "8px 16px",
+  marginTop: "16px",
+  width: "115px",
+}));
+
+const UploadButton = styled(ButtonStyle)(() => ({
+  borderRadius: "8px",
+  width: "180px",
+  marginTop: "0px",
+  marginRight: "5px",
+}));
+
+const OutlineButtonStyle = styled(ButtonStyle)(() => ({
+  color: "#616161",
+  border: "1px solid #F48FB1",
 }));
 
 const TextFieldStyle = styled(TextField)(() => ({
   width: "350px",
   Height: "36px",
   marginBottom: "16px",
-  "& .MuiOutlinedInput-root": {
+  "& .MuilinedInput-root": {
     "& fieldset": {
       borderRadius: "8px",
     },
+  },
+}));
+
+const MultilineTextFieldStyle = styled(TextField)(() => ({
+  width: "600px",
+  "& .css-ivu46s-MuiInputBase-root-MuiOutlinedInput-root": {
+    padding: "0px",
+  },
+  "& .css-1h8391f-MuiFormHelperText-root": {
+    marginLeft: "0px",
+    fontSize: "14px",
+  },
+  "& .css-1h8391f-MuiFormHelperText-root ": {
+    color: "#8E8E8E",
   },
 }));
 
@@ -54,18 +84,18 @@ const Auth = () => {
     <Stack width="100%" alignItems="center">
       <Stack
         direction="row"
-        padding="32px"
+        padding="0px"
         width="100%"
         height="100%"
         maxWidth="1440px"
         maxHeight="900px"
         justifyContent="center"
       >
-        <Box flex={1} p={10}>
+        <Box flex={1} p={10} paddingLeft="188px">
           <Typography variant="h3" marginBottom="16px" color="warning">
             Good choice!
           </Typography>
-          <Typography variant="h6" marginBottom="32px" color="warining">
+          <Typography variant="h6" marginBottom="18px" color="warining">
             Create a new account as...
           </Typography>
           <Tabs
@@ -99,7 +129,8 @@ const Auth = () => {
             />
           </Tabs>
           {/* Form */}
-          <Box
+          {/*---------- Form1 ------------*/}
+          {/* <Box
             marginTop="36px"
             component="form"
             sx={{ width: "360px" }}
@@ -109,25 +140,198 @@ const Auth = () => {
             <Box>
               <InputLabelStyle>EMAIL</InputLabelStyle>
               <TextFieldStyle
+                value=""
                 label=""
                 color="primary"
                 placeholder="some.user@mail.com"
                 focused
                 inputProps={{ style: { padding: 8 } }}
               />
-              <InputLabelStyle>EMAIL</InputLabelStyle>
+              <InputLabelStyle>PASSWORD</InputLabelStyle>
               <TextFieldStyle
+                value=""
                 label=""
                 color="primary"
-                placeholder="some.user@mail.com"
+                placeholder="******"
                 focused
                 inputProps={{ style: { padding: 8 } }}
               />
+              <InputLabelStyle>PASSWORD CONFIRMATION</InputLabelStyle>
+              <TextFieldStyle
+                value=""
+                label=""
+                color="primary"
+                placeholder="******"
+                focused
+                inputProps={{ style: { padding: 8 } }}
+              />
+              <Stack display="flex" alignItems="center">
+                <ButtonStyle variant="contained" color="primary">
+                  Next >
+                </ButtonStyle>
+              </Stack>
+            </Box>
+          </Box> */}
+          {/*---------- Form2 ------------*/}
+          {/* <Box
+            marginTop="36px"
+            component="form"
+            sx={{ width: "360px" }}
+            noValidate
+            autoComplete="off"
+          >
+            <Box>
+              <InputLabelStyle>NAME</InputLabelStyle>
+              <TextFieldStyle
+                value=""
+                label=""
+                color="primary"
+                placeholder="John Doe"
+                focused
+                inputProps={{ style: { padding: 8 } }}
+              />
+              <InputLabelStyle>PHONE</InputLabelStyle>
+              <TextFieldStyle
+                value=""
+                label=""
+                color="primary"
+                placeholder="+xxxxxxxxx"
+                focused
+                inputProps={{ style: { padding: 8 } }}
+              />
+              <InputLabelStyle>BIRTHDAY</InputLabelStyle>
+              <TextFieldStyle
+                value=""
+                label=""
+                color="primary"
+                placeholder="Pick a date"
+                focused
+                inputProps={{ style: { padding: 8 } }}
+              />
+              <InputLabelStyle>LINKEDIN URL</InputLabelStyle>
+              <TextFieldStyle
+                value=""
+                label=""
+                color="primary"
+                placeholder="https://www.linkedin.com/in/username"
+                focused
+                inputProps={{ style: { padding: 8 } }}
+              />
+              <Stack
+                direction="row"
+                display="flex"
+                justifyContent="center"
+                gap="15px"
+              >
+                <OutlineButtonStyle variant="outlined" color="primary">
+                  SKIP THIS!
+                </OutlineButtonStyle>
+                <ButtonStyle variant="contained" color="primary">
+                  Next >
+                </ButtonStyle>
+              </Stack>
+            </Box>
+          </Box> */}
+          {/*---------- Form3 ------------*/}
+          <Box
+            marginTop="36px"
+            component="form"
+            sx={{ width: "360px" }}
+            noValidate
+            autoComplete="off"
+          >
+            <Box>
+              <InputLabelStyle>TITLE</InputLabelStyle>
+              <TextFieldStyle
+                defaultValue=""
+                label=""
+                color="primary"
+                placeholder="Mechanical administrator..."
+                focused
+                inputProps={{ style: { padding: 8 } }}
+              />
+              <InputLabelStyle>PROFESSIONAL EXPERIENCE</InputLabelStyle>
+              <MultilineTextFieldStyle
+                defaultValue=""
+                style={{ marginBottom: "14px" }}
+                label=""
+                color="primary"
+                placeholder="Worked 6 years in a bitcoin farm until I decided to change my life..."
+                focused
+                inputProps={{ style: { padding: "8px" } }}
+                helperText="Between 300 and 2000 characters"
+                multiline
+                rows={3}
+              />
+              <InputLabelStyle>EDUCATION</InputLabelStyle>
+              <MultilineTextFieldStyle
+                defaultValue=""
+                style={{ marginBottom: "14px" }}
+                label=""
+                color="primary"
+                placeholder="Major in life experiences with a PHD in procrastination..."
+                focused
+                inputProps={{ style: { padding: "8px" } }}
+                helperText="Between 100 and 2000 characters"
+                multiline
+                rows={3}
+              />
+
+              <InputLabelStyle>UPLOAD/UPDATE YOUR CV</InputLabelStyle>
+              <UploadButton
+                startIcon={<FileUploadOutlined />}
+                variant="contained"
+                component="label"
+                color="primary"
+                helperText="hi"
+                position="relative"
+              >
+                Choose a file
+                <input
+                  left="10px"
+                  hidden
+                  width="300px"
+                  accept="image/*"
+                  multiple
+                  type="file"
+                />
+              </UploadButton>
+
+              <Typography
+                component="span"
+                variant="body2"
+                color="secondary.main"
+              >
+                No file chosen
+              </Typography>
+
+              <Typography
+                component="p"
+                variant="body2"
+                marginTop="4px"
+                color="info.main"
+                textTransform="none"
+              >
+                Only PDF. Max size 5MB
+              </Typography>
+              <Stack
+                direction="row"
+                display="flex"
+                justifyContent="center"
+                gap="15px"
+              >
+                <OutlineButtonStyle variant="outlined" color="primary">
+                  SKIP THIS!
+                </OutlineButtonStyle>
+                <ButtonStyle variant="contained" color="primary">
+                  Next >
+                </ButtonStyle>
+              </Stack>
             </Box>
           </Box>
         </Box>
         <Box flex={1}>
-          <ImageListItem sx={{ position: "absolute", bottom: "0px" }}>
+          <ImageListItem sx={{ position: "fixed", bottom: "0px" }}>
             <img src="images/discussing.svg" alt="woman" />
           </ImageListItem>
         </Box>
@@ -135,11 +339,5 @@ const Auth = () => {
     </Stack>
   );
 };
-
-{
-  /* <ButtonStyle variant="contained" color="primary">
-            Next >
-          </ButtonStyle> */
-}
 
 export default Auth;

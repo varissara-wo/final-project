9a9629b40fca710bbce772248bb9cb8e3e591eb2
Varissara <varissara.wo@gmail.com>
2022-11-03 22:45:45 +0700

    add Navbar

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -10,6 +10,8 @@ import {
   ImageListItem,
 } from "@mui/material";
 
+import Navbar from "../components/Navbar.jsx";
+
 const Auth = () => {
   //   <--------------- user type --------------->
 
@@ -22,66 +24,69 @@ const Auth = () => {
   //   <--------------- form type --------------->
 
   return (
-    <Stack width="100%" alignItems="center">
-      <Stack
-        direction="row"
-        padding="0px"
-        width="100%"
-        height="100%"
-        maxWidth="1440px"
-        maxHeight="900px"
-        justifyContent="center"
-      >
-        {/* -------------------------Form section------------------ */}
-        <Box flex={1} p={10} paddingLeft="188px">
-          <Typography variant="h3" marginBottom="16px" color="warning">
-            Good choice!
-          </Typography>
-          <Typography variant="h6" marginBottom="18px" color="warining">
-            Create a new account as...
-          </Typography>
-          <Tabs
-            color="Pink"
-            value={userType}
-            onChange={handleChange}
-            textColor="secondary"
-            indicatorColor="primary"
-            aria-label="secondary tabs example"
-          >
-            <Tab
-              value="professional"
-              label="PROFESSIONAL"
-              sx={{
-                fontFamily: "var(--inter-font)",
-                fontWeight: "500",
-                borderBottom: "2px solid #BDBDBD",
-                padding: "0px",
-              }}
-            />
-            <Tab
-              value="recruiter"
-              label="RECRUITER"
-              sx={{
-                fontFamily: "var(--inter-font)",
-                fontWeight: "500",
-                borderBottom: "2px solid #BDBDBD",
-                padding: "0px",
-                marginLeft: "16px",
-              }}
-            />
-          </Tabs>
+    <>
+      <Navbar />
+      <Stack width="100%" alignItems="center">
+        <Stack
+          direction="row"
+          padding="0px"
+          width="100%"
+          height="100%"
+          maxWidth="1440px"
+          maxHeight="900px"
+          justifyContent="center"
+        >
+          {/* -------------------------Form section------------------ */}
+          <Box flex={1} p={10} paddingLeft="188px">
+            <Typography variant="h3" marginBottom="16px" color="warning">
+              Good choice!
+            </Typography>
+            <Typography variant="h6" marginBottom="18px" color="warining">
+              Create a new account as...
+            </Typography>
+            <Tabs
+              color="Pink"
+              value={userType}
+              onChange={handleChange}
+              textColor="secondary"
+              indicatorColor="primary"
+              aria-label="secondary tabs example"
+            >
+              <Tab
+                value="professional"
+                label="PROFESSIONAL"
+                sx={{
+                  fontFamily: "var(--inter-font)",
+                  fontWeight: "500",
+                  borderBottom: "2px solid #BDBDBD",
+                  padding: "0px",
+                }}
+              />
+              <Tab
+                value="recruiter"
+                label="RECRUITER"
+                sx={{
+                  fontFamily: "var(--inter-font)",
+                  fontWeight: "500",
+                  borderBottom: "2px solid #BDBDBD",
+                  padding: "0px",
+                  marginLeft: "16px",
+                }}
+              />
+            </Tabs>
 
-          {/* -------------------------Stepper Professional------------------ */}
-          {userType === "professional" && <Professional />}
-          {userType === "recruiter" && <Recruiter />}
-        </Box>
-        <Box flex={1}>
-          <ImageListItem sx={{ position: "fixed", bottom: "0px" }}>
-            <img src="images/discussing.svg" alt="woman" />
-          </ImageListItem>
-        </Box>
+            {/* -------------------------Stepper Professional------------------ */}
+            {userType === "professional" && <Professional />}
+            {userType === "recruiter" && <Recruiter />}
+          </Box>
+          <Box flex={1}>
+            <ImageListItem sx={{ position: "fixed", bottom: "0px" }}>
+              <img src="images/discussing.svg" alt="woman" />
+            </ImageListItem>
+          </Box>
+        </Stack>
       </Stack>
-    </Stack>
+    </>
   );
 };
 

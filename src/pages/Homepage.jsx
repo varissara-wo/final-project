c5e8a27dc41ecc25deaec041fedaf8ecc19714e9
hohokz <hohokz@hotmail.com>
2022-11-03 10:32:38 +0700

    feat: nav bar finish

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,16 +6,20 @@ import Box from '@mui/material/Box';
 const Homepage = () => {
     return (
         <>
-
-            <NavBox />
             <Box sx={{
-                margin: '0px',
-                width: 'auto',
-                height: '897px',
-                background: '#E5E5E5'
+
             }}>
+                <NavBox />
+            </Box>
+
+            <Box margin='0px' width='auto' height='897px'
+                sx={{
+                    backgroundColor: '#E5E5E5'
+                }} >
+
 
             </Box>
+
 
         </>
     )
--- src/theme.js
+++ src/theme.js
@@ -127,6 +127,9 @@ export const theme = createTheme({
       fontSize: "16px",
     },
   },
+  shadows: ["#000000"]
 });
+
+
 
 

d48dd2486ea3ea13dc3d47b9b76c172a9c02dd36
Varissara <varissara.wo@gmail.com>
2022-11-15 20:10:07 +0700

    feat: create job post, style: responsive background and fixed sidebar and refactor: divide component in jop post page

---
+++ clients/src/components/Recruiter/OnelineInputJobPost.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { OnelineTextField, InputLabelStyle } from "../Register/Styles.jsx";
+
+const OnelineInputJobPost = (props) => {
+  const { label, name, onChange, value, placeholder } = props;
+  return (
+    <>
+      <InputLabelStyle style={{ fontSize: "10px", fontWeight: "500" }}>
+        {label}
+      </InputLabelStyle>
+      <OnelineTextField
+        defaultValue=""
+        color="primary"
+        focused
+        inputProps={{ style: { padding: 8 } }}
+        name={name}
+        value={value}
+        type="text"
+        placeholder={placeholder}
+        onChange={onChange}
+        style={{ width: "360px ", marginBottom: "4px" }}
+      />
+    </>
+  );
+};
+
+export default OnelineInputJobPost;
